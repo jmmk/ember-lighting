06b595c01036dec764b986164adc9e0b9bd827b6
Michael McLellan <jmikem825@gmail.com>
2013-10-02 15:57:00 -0400

    Complete Turn all on/off

--- app/controllers/light.js
+++ app/controllers/light.js
@@ -1,7 +1,6 @@
 var LightController = Ember.ObjectController.extend({
   actions: {
     toggle: function(light) {
-
       light.set('isOn', !light.get('isOn'));
     }
   }
--- app/controllers/lights.js
+++ app/controllers/lights.js
@@ -1,4 +1,12 @@
 var LightsController = Ember.ArrayController.extend({
+  actions: {
+    turnOn: function(lights) {
+      this.setEach('isOn', true);
+    },
+    turnOff: function(lights) {
+      this.setEach('isOn', false);
+    }
+  }
 });
 
 export default LightsController;
